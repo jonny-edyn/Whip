3e8958e4df782c5537b8c3952f83f4354b9a884f
Alan Locke <jlocke2288@gmail.com>
2015-04-23 08:57:26 +0200

    Added Error Pages.  Need To Style Them Still

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,10 @@ Rails.application.routes.draw do
   get '/my_votes' => 'votes#my_votes'
   resources :votes
 
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
