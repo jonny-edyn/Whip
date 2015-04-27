2c3f7dd2dae69551b77b8e0d977e2bccbc64fab4
Alan Locke <jlocke2288@gmail.com>
2015-04-27 08:30:10 -0400

    Fixed MP Load To Include All Details Available and Fixed Landing Page Form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,18 @@
 class StaticPagesController < ApplicationController
 
 	def home
-		
+
 	end
 
 	def mailing_list_request
 		@contact_email = params[:contact_email]
 
 		Resque.enqueue(AddToMailingList, @contact_email)
-		redirect_to :back
+
+		respond_to do |format|
+			format.html redirect_to :back
+			format.js { render :partial => 'add_user_to_mailing_list.js.erb' }
+		end
 	end
 
 	
