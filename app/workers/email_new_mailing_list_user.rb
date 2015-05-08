11e729dbdf44ff98d18639ebaed9f33706f52ecc
Alan Locke <jlocke2288@gmail.com>
2015-05-08 02:48:38 +0200

    Finalized welcome email

--- app/workers/email_new_mailing_list_user.rb
+++ app/workers/email_new_mailing_list_user.rb
@@ -58,9 +58,38 @@ class EmailNewMailingListUser
 		<p>Thanks,</p>
 		<p>The Whip Team</p>
 	  </div>
-	  <div style='text-align:center;border-bottom: #d2d2d2 1px solid; width:100%;'>
-	  	<img style='width:35%;' src='http://www.whip.org.uk/assets/bottom_of_email-06c57391e071abe5d20ffa4e81a64a02.png' alt='My image' />
+
+	  <div style='text-align:center; width:100%;'>
+	  	<img style='width:35%;' src='http://www.whip.org.uk/assets/bottom_of_email_logo-8d574e6e6f6a05c17b108297719e75b1.png' alt='My image' />
 	  </div>
+
+
+
+	 <div style='text-align:center; width:100%;'>
+	 	<div style='width: auto;display:inline-block;padding: 10px;'>
+			<a href='https://www.facebook.com/whipuk' target='_blank'><img src='http://www.whip.org.uk/assets/fb_icon-77e79838462dfb06153ec7dba9dd1301.png'></a>
+		</div>
+		<div style='width: auto;display:inline-block;padding: 10px;'>
+			<a href='https://twitter.com/whipuk' target='_blank'><img src='http://www.whip.org.uk/assets/tw_icon-9e3911be9aa26638ea6aec45da76187b.png'></a>
+		</div>
+		<div style='width: auto;display:inline-block;padding: 10px;'>
+			<a href='https://instagram.com/whipuk/' target='_blank'><img src='http://www.whip.org.uk/assets/insta_icon-815ef56e006ac03a77335fcb9ee52197.png'></a>
+		</div>
+		<div style='width: auto;display:inline-block;padding: 10px;'>
+			<a href='https://www.youtube.com/channel/UCdt6hdrjwrRB9hLzvUdl5Lw' target='_blank'><img src='http://www.whip.org.uk/assets/youtube_icon-1f5090b500f609279a516bbdf25dcfb3.png'></a>
+		</div>
+		<div style='width: auto;display:inline-block;padding: 10px;'>
+			<a href='http://whipuk.tumblr.com/' target='_blank'><img src='http://www.whip.org.uk/assets/tumblr_icon-4f35e3c96f52b9416a6f1532e4217b85.png'></a>
+		</div>
+	 </div>
+	
+
+
+
+	  <div style='text-align:center; width:100%;'>
+	  	<img style='width:35%;' src='http://www.whip.org.uk/assets/bottom_of_email_address-94f5a19f70f2e0cacd03dac6700d05e8.png' alt='My image' />
+	  </div>
+
 	 </html>",  
 	 :from_email=>"contact@whip.org.uk"  
 	}  
