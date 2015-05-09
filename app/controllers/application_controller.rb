7cf419f3a2024c2e2c8377c6ae0dcbee5255ffea
cokemorgan <cmorgan@fellowshipknox.org>
2015-05-09 12:38:27 -0400

    bills trending

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::Base
       "blank"
     elsif controller_name == 'static_pages' && action_name == 'home'
       "blank"
-    elsif controller_name == 'static_pages' && action_name == 'postlaunch_landing_page'
-      "blank"
     elsif controller_name == 'static_pages' && action_name == 'terms'
       "blank"
     elsif controller_name == 'static_pages' && action_name == 'privacy'
+      "blank"
+    elsif controller_name == 'bills' && action_name == 'index'
       "blank"
     else
       "application"
