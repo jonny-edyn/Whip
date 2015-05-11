9130550cb4607d61127b39bbf1533abdc210457f
Alan Locke <jlocke2288@gmail.com>
2015-05-11 20:24:00 +0200

    Custom Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  # Returns full title on a per page basis
+  def full_title(page_title)
+  	base_title = "Whip"
+  	if page_title.empty?
+  		base_title
+  	else
+  		"#{base_title} | #{page_title}"
+  	end
+  end
+
 end
