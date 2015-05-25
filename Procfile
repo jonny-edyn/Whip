dcd58e4945fbb6bbc2c4623711c2b23c6a14fb44
Alan Locke <jlocke2288@gmail.com>
2015-05-25 18:51:11 +0300

    Still trying to get max connections to work

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-resque: env TERM_CHILD=1 COUNT=1 QUEUE=* RESQUE_TERM_TIMEOUT=30 VVERBOSE=1 bundle exec rake resque:work
+resque: env TERM_CHILD=1 COUNT=2 QUEUE=* RESQUE_TERM_TIMEOUT=30 VVERBOSE=1 bundle exec rake resque:work
--- app/workers/mps_set.rb
+++ app/workers/mps_set.rb
@@ -8,16 +8,6 @@ class MpsSet
 
 	constituencies = Constituency.all
 	constituencies.each_with_index do |constituency, index|
-		if index == 0
-			@setting = Setting.find_by(name: 'updating_mp_list')
-				@setting.yes = true
-			@setting.save
-		elsif index == constituencies.size - 1
-			@setting = Setting.find_by(name: 'updating_mp_list')
-				@setting.yes = false
-			@setting.save
-		end
-				
 		Resque.enqueue(AddMp, constituency.id)
   	end
   end
