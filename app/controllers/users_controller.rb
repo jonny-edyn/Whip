a9a400c7722170c45dcde48398a01d1a854e5a34
Alan Locke <jlocke2288@gmail.com>
2015-06-22 18:09:29 +0200

    Final Final Round Of Edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         sign_in(@user == current_user ? @user : current_user, bypass: true)
-        format.html { redirect_to :back, notice: 'Your profile was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Your profile was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
